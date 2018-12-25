a9990f3f06de786166d008af64cebecbc2b16c57
HowsonLiu <948470636@qq.com>
2018-12-25 22:20:11 +0800

    添加针对各种注释格式的添加与撤销 测试成功

--- CopyrightAppender/CopyrightAppender.py
+++ CopyrightAppender/CopyrightAppender.py
@@ -1,5 +1,6 @@
 import os
 import configparser
+import re
 
 class CopyrightAppender:
     ini_path = './CopyrightAppender.ini'
@@ -80,18 +81,20 @@ class CopyrightAppender:
     def __choose_append_style(self, path):
         for suffix in self.c_style_suffix:
             if path.endswith(suffix):
-                self.__append_c(path)
+                self.__clean_c(path)
                 return
         for suffix in self.python_style_suffix:
             if path.endswith(suffix):
-                self.__comment_on_py()
+                #self.__append_py(path)
+                self.__clean_py(path)
                 return
         for suffix in self.ini_style_suffix:
             if path.endswith(suffix):
-                self.__comment_on_ini()
+                self.__clean_ini(path)
                 return
-        self.__comment_on_text()
+        self.__clean_text(path)
 
+# ------------------------------------------C类型注释---------------------------------------------
     def __comment_on_c(self):
         c_comment = []
         for line in self.copyright_text_line:
@@ -109,27 +112,121 @@ class CopyrightAppender:
             file.write(copyright_text)
             file.write(oldtext)
 
+    def __clean_c(self, path):
+        copyright_text = self.__comment_on_c()
+        with open(path, 'r') as file:
+            text = file.read()
+        if copyright_text in text:
+            text = text.replace(copyright_text, '')
+            with open(path, 'w') as file:
+                file.write(text)
+            return True
+        return False
 
+# -------------------------------------------------------------------------------------------------
+
+# -----------------------------------------Python类型注释------------------------------------------
+
+    # 两个返回值，第一个是字符串，第二个是行列表
     def __comment_on_py(self):
         py_comment = []
         for line in self.copyright_text_line:
             py_comment.append('# ' + line)
         py_comment.append('\n\n# Add by CopyrightAppender\n')
-        print(''.join(py_comment))
+        return ''.join(py_comment), py_comment
+
+    # python不能直接插入第一行，有可能有特殊注释，特殊注释一般在一二行，我只对一二行进行判断
+    def __append_py(self, path):
+        copyright_text, copyright_text_line = self.__comment_on_py()
+        with open(path, 'r+') as file:
+            file_line = file.readlines()
+            insert_num = 0
+            if len(file_line) >= 1:
+                # 第一行通常是编译器路径
+                if re.compile('^#!').search(file_line[0]) is not None:
+                    insert_num += 1
+                # 第二行是编码格式 https://blog.csdn.net/xld_19920728/article/details/80534146
+                if re.compile('^[ \t\v]*#.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)').search(file_line[0]) is not None:
+                    insert_num += 1
+                if len(file_line) >= 2 and re.compile('^[ \t\v]*#.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)').search(file_line[1]) is not None:
+                    insert_num += 1
+            for t in reversed(copyright_text_line):
+                file_line.insert(insert_num, t)
+            file.seek(0)
+            file.write(''.join(file_line))
+
+    def __clean_py(self, path):
+        copyright_text, copyright_text_line = self.__comment_on_py()
+        with open(path, 'r') as file:
+            text = file.read()
+        if copyright_text in text:
+            text = text.replace(copyright_text, '')
+            with open(path, 'w') as file:
+                file.write(text)
+            return True
+        return False
+
+# -------------------------------------------------------------------------------------------------
+
+# ----------------------------------------ini类型注释-----------------------------------------------
 
     def __comment_on_ini(self):
         ini_comment = []
         for line in self.copyright_text_line:
             ini_comment.append(';' + line)
         ini_comment.append('\n\n;Add by CopyrightAppender\n')
-        print(''.join(ini_comment))
+        return ''.join(ini_comment)
+
+    def __append_ini(self, path):
+        copyright_text = self.__comment_on_ini()
+        with open(path, 'r+') as file:
+            oldtext = file.read()
+            file.seek(0)
+            file.write(copyright_text)
+            file.write(oldtext)
+
+    def __clean_ini(self, path):
+        copyright_text = self.__comment_on_ini()
+        with open(path, 'r') as file:
+            text = file.read()
+        if copyright_text in text:
+            text = text.replace(copyright_text, '')
+            with open(path, 'w') as file:
+                file.write(text)
+            return True
+        return False
+
+# ------------------------------------------------------------------------------------------------
+
+# ----------------------------------------txt类型注释-----------------------------------------------
 
     def __comment_on_text(self):
         text_comment = []
         for line in self.copyright_text_line:
             text_comment.append(line)
         text_comment.append('\n\nAdd by CopyrightAppender\n')
-        print(''.join(text_comment))
+        return ''.join(text_comment)
+
+    def __append_text(self, path):
+        copyright_text = self.__comment_on_text()
+        with open(path, 'r+') as file:
+            oldtext = file.read()
+            file.seek(0)
+            file.write(copyright_text)
+            file.write(oldtext)
+
+    def __clean_text(self, path):
+        copyright_text = self.__comment_on_text()
+        with open(path, 'r') as file:
+            text = file.read()
+        if copyright_text in text:
+            text = text.replace(copyright_text, '')
+            with open(path, 'w') as file:
+                file.write(text)
+            return True
+        return False
+
+# ------------------------------------------------------------------------------------------------
 
     def Work(self):
         if self.__check_file_exist() == 0:
